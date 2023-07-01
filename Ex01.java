54e8d5002f9ea5d90cea75a8af76f88f800b486d
boksem <nasu0210@naver.com>
2023-07-01 14:25:37 +0900

    7월1일 커밋

---
+++ Ex01.java
@@ -0,0 +1,9 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("7월1일 자바테스트..");
+
+	}
+
+}
---
+++ src/p0701/Ex01.java
@@ -0,0 +1,10 @@
+package p0701;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("7월1일 자바테스트...");
+
+	}
+
+}
